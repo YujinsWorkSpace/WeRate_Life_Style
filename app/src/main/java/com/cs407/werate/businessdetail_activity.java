b78d373e9d0b1f0af66058820e12d5e69a52b754
RobbieXXx <txu92@wisc.edu>
2023-11-23 23:13:07 -0600

    add detail screen

---
+++ app/src/main/java/com/cs407/werate/businessdetail_activity.java
@@ -0,0 +1,124 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		businessdetail
+	 *	@date 		Friday 24th of November 2023 04:09:43 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.cs407.werate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class businessdetail_activity extends Activity {
+
+	
+	private View _bg__businessdetail_ek2;
+	private View _bg____container_slider_default__slider_indicator__cdbaac__slider_infinite__ek1;
+	private View _bg__slide_1_ek1;
+	private ImageView platter_gab742ad7c_1920_1;
+	private View _bg__slide_2_ek1;
+	private ImageView platter_gab742ad7c_1920_1_ek1;
+	private View _bg__slide_3_ek1;
+	private ImageView platter_gab742ad7c_1920_1_ek2;
+	private View _bg____container__ek1;
+	private TextView business_name;
+	private TextView _____;
+	private TextView number_of_reviews;
+	private TextView category;
+	private TextView address;
+	private TextView ______;
+	private View rectangle_215;
+	private View _bg__write_ek1;
+	private TextView write_message;
+	private ImageView vector;
+	private View _bg____call_action_phone__34123456789__ek1;
+	private TextView call;
+	private ImageView vector_ek1;
+	private View _bg____web_action_openurl_https___www_bravostudio_app__ek1;
+	private TextView visit_website;
+	private ImageView vector_ek2;
+	private View _bg____container__ek3;
+	private View _bg__group_427319990_ek1;
+	private View rectangle_218;
+	private TextView btntxt;
+	private TextView reviews;
+	private View _bg____reviews_container__ek1;
+	private TextView review_text_flexo_;
+	private TextView author;
+	private TextView _______ek1;
+	private View rectangle_216;
+	private View _bg__topbar_container_top_bar__ek1;
+	private View rectangle_1;
+	private TextView yalp;
+	private ImageView vector_ek3;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.businessdetail);
+
+		
+		_bg__businessdetail_ek2 = (View) findViewById(R.id._bg__businessdetail_ek2);
+		_bg____container_slider_default__slider_indicator__cdbaac__slider_infinite__ek1 = (View) findViewById(R.id._bg____container_slider_default__slider_indicator__cdbaac__slider_infinite__ek1);
+
+		platter_gab742ad7c_1920_1 = (ImageView) findViewById(R.id.platter_gab742ad7c_1920_1);
+		_bg__slide_2_ek1 = (View) findViewById(R.id._bg__slide_2_ek1);
+		platter_gab742ad7c_1920_1_ek1 = (ImageView) findViewById(R.id.platter_gab742ad7c_1920_1_ek1);
+		_bg__slide_3_ek1 = (View) findViewById(R.id._bg__slide_3_ek1);
+		platter_gab742ad7c_1920_1_ek2 = (ImageView) findViewById(R.id.platter_gab742ad7c_1920_1_ek2);
+		_bg____container__ek1 = (View) findViewById(R.id._bg____container__ek1);
+		business_name = (TextView) findViewById(R.id.business_name);
+		_____ = (TextView) findViewById(R.id._____);
+		number_of_reviews = (TextView) findViewById(R.id.number_of_reviews);
+		category = (TextView) findViewById(R.id.category);
+		address = (TextView) findViewById(R.id.address);
+		______ = (TextView) findViewById(R.id.______);
+		rectangle_215 = (View) findViewById(R.id.rectangle_215);
+		_bg__write_ek1 = (View) findViewById(R.id._bg__write_ek1);
+		write_message = (TextView) findViewById(R.id.write_message);
+		vector = (ImageView) findViewById(R.id.vector);
+		_bg____call_action_phone__34123456789__ek1 = (View) findViewById(R.id._bg____call_action_phone__34123456789__ek1);
+		call = (TextView) findViewById(R.id.call);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		_bg____web_action_openurl_https___www_bravostudio_app__ek1 = (View) findViewById(R.id._bg____web_action_openurl_https___www_bravostudio_app__ek1);
+		visit_website = (TextView) findViewById(R.id.visit_website);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		_bg____container__ek3 = (View) findViewById(R.id._bg____container__ek3);
+		_bg__group_427319990_ek1 = (View) findViewById(R.id._bg__group_427319990_ek1);
+		rectangle_218 = (View) findViewById(R.id.rectangle_218);
+		btntxt = (TextView) findViewById(R.id.btntxt);
+		reviews = (TextView) findViewById(R.id.reviews);
+		_bg____reviews_container__ek1 = (View) findViewById(R.id._bg____reviews_container__ek1);
+		review_text_flexo_ = (TextView) findViewById(R.id.review_text_flexo_);
+		author = (TextView) findViewById(R.id.author);
+		_______ek1 = (TextView) findViewById(R.id._______ek1);
+		rectangle_216 = (View) findViewById(R.id.rectangle_216);
+		_bg__topbar_container_top_bar__ek1 = (View) findViewById(R.id._bg__topbar_container_top_bar__ek1);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		yalp = (TextView) findViewById(R.id.yalp);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
