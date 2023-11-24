28364365bf80d1ada4ceb8e254ca4a7192b7821a
RobbieXXx <txu92@wisc.edu>
2023-11-24 17:25:53 -0600

    update

--- app/src/main/java/com/cs407/werate/profile_activity.java
+++ app/src/main/java/com/cs407/werate/profile_activity.java
@@ -110,7 +110,7 @@ public class profile_activity extends Activity {
 		setContentView(R.layout.profile);
 
 		
-		_bg__profile_ek2 = (View) findViewById(R.id._bg__profile_ek2);
+
 		_bg__profile_ek4 = (View) findViewById(R.id._bg__profile_ek4);
 		profile_picture = (ImageView) findViewById(R.id.profile_picture);
 		name = (TextView) findViewById(R.id.name);
@@ -125,27 +125,22 @@ public class profile_activity extends Activity {
 		vector = (ImageView) findViewById(R.id.vector);
 		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
 		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
-		_bg__system_icon_24px_right_ek1 = (View) findViewById(R.id._bg__system_icon_24px_right_ek1);
+
 		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
 		_bg__birthday_ek1 = (View) findViewById(R.id._bg__birthday_ek1);
 		rectangle_392_ek1 = (View) findViewById(R.id.rectangle_392_ek1);
 		birthday_ek2 = (TextView) findViewById(R.id.birthday_ek2);
 		_12_12_2000 = (TextView) findViewById(R.id._12_12_2000);
-		_bg__system_icon_24px_date_ek1 = (View) findViewById(R.id._bg__system_icon_24px_date_ek1);
 		_bg__group_ek3 = (View) findViewById(R.id._bg__group_ek3);
 		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
 		_bg__system_icon_24px_right_ek3 = (View) findViewById(R.id._bg__system_icon_24px_right_ek3);
-		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
 		_bg__email_ek1 = (View) findViewById(R.id._bg__email_ek1);
 		rectangle_392_ek2 = (View) findViewById(R.id.rectangle_392_ek2);
 		email_ek2 = (TextView) findViewById(R.id.email_ek2);
 		derlaxy_yahoo_com = (TextView) findViewById(R.id.derlaxy_yahoo_com);
-		_bg__system_icon_24px_message_ek1 = (View) findViewById(R.id._bg__system_icon_24px_message_ek1);
 		_bg__group_ek5 = (View) findViewById(R.id._bg__group_ek5);
 		vector__stroke_ = (ImageView) findViewById(R.id.vector__stroke_);
 		vector__stroke__ek1 = (ImageView) findViewById(R.id.vector__stroke__ek1);
-		_bg__system_icon_24px_right_ek5 = (View) findViewById(R.id._bg__system_icon_24px_right_ek5);
-		vector_ek6 = (ImageView) findViewById(R.id.vector_ek6);
 		_bg__phone_number_ek1 = (View) findViewById(R.id._bg__phone_number_ek1);
 		rectangle_392_ek3 = (View) findViewById(R.id.rectangle_392_ek3);
 		phone_number_ek2 = (TextView) findViewById(R.id.phone_number_ek2);
@@ -154,8 +149,6 @@ public class profile_activity extends Activity {
 		_bg__group_ek7 = (View) findViewById(R.id._bg__group_ek7);
 		vector_ek7 = (ImageView) findViewById(R.id.vector_ek7);
 		vector_ek8 = (ImageView) findViewById(R.id.vector_ek8);
-		_bg__system_icon_24px_right_ek7 = (View) findViewById(R.id._bg__system_icon_24px_right_ek7);
-		vector_ek9 = (ImageView) findViewById(R.id.vector_ek9);
 		_bg__change_password_ek1 = (View) findViewById(R.id._bg__change_password_ek1);
 		rectangle_392_ek4 = (View) findViewById(R.id.rectangle_392_ek4);
 		change_password_ek2 = (TextView) findViewById(R.id.change_password_ek2);
@@ -165,8 +158,6 @@ public class profile_activity extends Activity {
 		vector__stroke__ek2 = (ImageView) findViewById(R.id.vector__stroke__ek2);
 		vector__stroke__ek3 = (ImageView) findViewById(R.id.vector__stroke__ek3);
 		vector__stroke__ek4 = (ImageView) findViewById(R.id.vector__stroke__ek4);
-		_bg__system_icon_24px_right_ek9 = (View) findViewById(R.id._bg__system_icon_24px_right_ek9);
-		vector_ek10 = (ImageView) findViewById(R.id.vector_ek10);
 		_bg__header_ek1 = (View) findViewById(R.id._bg__header_ek1);
 		rectangle_406 = (View) findViewById(R.id.rectangle_406);
 		profile_ek5 = (TextView) findViewById(R.id.profile_ek5);
@@ -174,8 +165,7 @@ public class profile_activity extends Activity {
 		_bg__system_icon_24px_left_ek1 = (View) findViewById(R.id._bg__system_icon_24px_left_ek1);
 		vector_ek11 = (ImageView) findViewById(R.id.vector_ek11);
 		_bg__ui___status_bars_ek1 = (View) findViewById(R.id._bg__ui___status_bars_ek1);
-		_bg__ui___home_indicator_ek1 = (View) findViewById(R.id._bg__ui___home_indicator_ek1);
-		home_indicator = (View) findViewById(R.id.home_indicator);
+
 	
 		
 		//custom code goes here
