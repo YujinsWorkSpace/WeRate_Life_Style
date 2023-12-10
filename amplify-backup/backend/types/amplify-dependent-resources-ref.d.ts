56bab39d58dd5274b1e62d9a06517da2fa454de5
RobbieXXx <txu92@wisc.edu>
2023-12-10 17:00:47 -0600

    add forgotPassword

---
+++ amplify-backup/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "WeRateApi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "weratelifestyle965b3899": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3weratelifestylestorage6440478b": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
